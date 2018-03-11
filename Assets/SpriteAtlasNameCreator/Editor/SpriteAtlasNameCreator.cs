3fce18e6e44a9ae610cc1d947c5ebf0e72c026b6
baba_s <shotababa1206@gmail.com>
2018-03-11 16:05:38 +0900

    書式指定を可能に

--- Assets/SpriteAtlasNameCreator/Editor/SpriteAtlasNameCreator.cs
+++ Assets/SpriteAtlasNameCreator/Editor/SpriteAtlasNameCreator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnityEngine;
 using UnityEngine.U2D;
 
@@ -9,6 +10,51 @@ namespace KoganeUnityLib
 {
 	public static class SpriteAtlasNameCreator
 	{
+		private sealed class FormatData
+		{
+			private readonly string m_format    ;
+			private readonly string m_name      ;
+			private readonly string m_argument  ;
+			private readonly string m_arg0      ;
+
+			public string Format    { get { return m_format     ; } }
+			public string Name      { get { return m_name       ; } }
+			public string Argument  { get { return m_argument   ; } }
+			public string Arg0      { get { return m_arg0       ; } }
+
+			/// <summary>
+			/// コンストラクタ
+			/// </summary>
+			public FormatData( int count, string name, string label )
+			{
+				m_format = name;
+
+				m_name = label
+					.Replace( "/", "_" )
+					.Trim( '_' )
+					.Replace( "__", "_" )
+				;
+
+				var range = Enumerable.Range( 0, count );
+
+				m_argument  = string.Join( ", ", range.Select( c => "object arg" + c    ).ToArray() );
+				m_arg0      = string.Join( ", ", range.Select( c => "arg" + c           ).ToArray() );
+			}
+		}
+
+		private sealed class FormatDataComparer : IEqualityComparer<FormatData>
+		{
+			public bool Equals( FormatData x, FormatData y )
+			{
+				return x.Name == y.Name;
+			}
+
+			public int GetHashCode( FormatData obj )
+			{
+				return obj.Name.GetHashCode();
+			}
+		}
+
 		private static readonly string[] INVALID_CHARS =
 		{
 			" ", "!", "\"", "#", "$",
@@ -65,6 +111,18 @@ namespace KoganeUnityLib
 				}
 
 				builder.Append( "\t\t" ).AppendLine();
+
+                var names = sprites.Select( c => c.name ).ToArray();
+
+                foreach ( var n in ToFormatDataSequence( names ).Distinct( new FormatDataComparer() ) )
+                {
+                    builder.Append( "\t\t" ).AppendFormat( @"/// <summary>""{0}""</summary>", n.Format ).AppendLine();
+                    builder.Append( "\t\t" ).AppendFormat( "public Sprite get_{0}( {1} ) {{ ", n.Name, n.Argument );
+                    builder.AppendFormat( @"return m_atlas.GetSprite( string.Format( ""{0}"", {1} ) ); ", n.Format, n.Arg0 );
+                    builder.AppendLine( "}" );
+                }
+
+				builder.Append( "\t\t" ).AppendLine();
 				builder.Append( "\t\t" ).AppendFormat( @"public {0}() {{}}", atlasNameSafe ).AppendLine();
 				builder.Append( "\t\t" ).AppendLine();
 				builder.Append( "\t\t" ).AppendFormat( @"public {0}( SpriteAtlas atlas ) {{ m_atlas = atlas; }}", atlasNameSafe ).AppendLine();
@@ -79,10 +137,68 @@ namespace KoganeUnityLib
 			return builder.ToString();
 		}
 
-		public static string RemoveInvalidChars( string str )
+		private static string RemoveInvalidChars( string str )
 		{
 			Array.ForEach( INVALID_CHARS, c => str = str.Replace( c, string.Empty ) );
 			return str;
+		}
+
+		private static IEnumerable<FormatData> ToFormatDataSequence( IEnumerable<string> spriteNames )
+		{
+			foreach ( var n in GetFormattableTextList( "_", spriteNames.ToArray() ) )
+			{
+				var matches = Regex.Matches( n, @"{[0-9]}" );
+
+				if ( matches.Count <= 0 ) continue;
+
+				var safeLabel = Regex.Replace( n, @"{[0-9]}", string.Empty );
+
+				if ( string.IsNullOrEmpty( safeLabel ) ) continue;
+
+				yield return new FormatData( matches.Count, n, safeLabel );
+			}
+		}
+
+		private static string[] GetFormattableTextList( string separator, params string[] texts )
+		{
+			var list = new List<string>();
+
+			for ( int i = 0; i < texts.Length - 1; i++ )
+			{
+				var separatedText1 = texts[ i ].Split( new []{ separator }, StringSplitOptions.None ).ToArray();
+
+				for ( int j = i + 1; j < texts.Length; j++ )
+				{
+					var separatedText2 = texts[ j ].Split( new []{ separator }, StringSplitOptions.None ).ToArray();
+
+					int argumentCount = 0;
+
+					var builder = new StringBuilder();
+
+					for ( int k = 0; k < Math.Min( separatedText1.Length, separatedText2.Length ); k++ )
+					{
+						var text1 = separatedText1[ k ];
+						var text2 = separatedText2[ k ];
+
+						if ( text1 == text2 )
+						{
+							builder.Append( text1 );
+						}
+						else
+						{
+							builder.AppendFormat( "{{{0}}}", argumentCount++ );
+						}
+
+						builder.Append( "_" );
+					}
+
+					builder = builder.Remove( builder.Length - 1, 1 );
+
+					list.Add( builder.ToString() );
+				}
+			}
+
+			return list.Distinct().ToArray();
 		}
 	}
 }
--- Assets/SpriteAtlasNameCreator/Example/Scripts/Example.cs
+++ Assets/SpriteAtlasNameCreator/Example/Scripts/Example.cs
@@ -9,20 +9,27 @@ namespace Example_SpriteAtlasNameCreator
 	{
 		public SpriteAtlas m_spriteAtlas;
 		public CommonAtlas m_commonAtlas;
-		public Image m_image;
+		public Image m_image1;
+		public Image m_image2;
+		public Image m_image3;
+		public Image m_image4;
 
 		private void Awake()
 		{
 			// 基本的な使い方
-			m_image.sprite = m_commonAtlas.button_blue;
+			m_image1.sprite = m_commonAtlas.button_blue;
+
+			// 書式指定も可能
+			var isOn = true;
+			m_image2.sprite = m_commonAtlas.get_check( isOn ? "on" : "off" );
 
 			// スプライト名を管理するクラスは動的に生成可能
 			var spaceAtlas = new SpaceAtlas( m_spriteAtlas );
-			m_image.sprite = spaceAtlas.dot_blue;
+			m_image3.sprite = spaceAtlas.dot_green;
 
 			// スプライト名を定数で参照することも可能
-			var name = SpaceAtlas._dot_blue;
-			m_image.sprite  = m_spriteAtlas.GetSprite( name );
+			var name = SpaceAtlas._dot_yellow;
+			m_image4.sprite = m_spriteAtlas.GetSprite( name );
 		}
 	}
 }
