4273ae32ed9086fa1df91a94d77b0997eecbbb24
baba_s <shotababa1206@gmail.com>
2018-03-11 15:51:30 +0900

    不要な画像削除

--- Assets/SpriteAtlasNameCreator/Example/Scripts/Example.cs
+++ Assets/SpriteAtlasNameCreator/Example/Scripts/Example.cs
@@ -14,14 +14,14 @@ namespace Example_SpriteAtlasNameCreator
 		private void Awake()
 		{
 			// 基本的な使い方
-			m_image.sprite = m_commonAtlas.blue_button00;
+			m_image.sprite = m_commonAtlas.button_blue;
 
 			// スプライト名を管理するクラスは動的に生成可能
 			var spaceAtlas = new SpaceAtlas( m_spriteAtlas );
-			m_image.sprite = spaceAtlas.dotBlue;
+			m_image.sprite = spaceAtlas.dot_blue;
 
 			// スプライト名を定数で参照することも可能
-			var name = SpaceAtlas._dotBlue;
+			var name = SpaceAtlas._dot_blue;
 			m_image.sprite  = m_spriteAtlas.GetSprite( name );
 		}
 	}
